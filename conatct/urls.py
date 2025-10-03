392311c04c93d8af8bb5e7972f546c34006ed824
Arathy <arathyrajeesh2@gmail.com>
2025-10-03 10:28:14 +0530

    contact added

--- conatct/urls.py
+++ conatct/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-
-from .import views
+from . import views
 
 urlpatterns = [
-    path('',views.home_view,name='home'),
+    path('', views.contact_list, name='contact_list'),
+    path('add/', views.add_contact, name='add_contact'),
+    path('edit/<int:pk>/', views.edit_contact, name='edit_contact'),
+    path('delete/<int:pk>/', views.delete_contact, name='delete_contact'),
+    path('adminhome/', views.admin_Dashboard, name='admin_dashboard'),
+    path('deletecontact/<int:pk>/', views.admin_contact, name='admin_contact'),
+    path('editcontact/<int:pk>/', views.edit_admin, name='edit_contact'),
+    
     
 ]
--- conatct/views.py
+++ conatct/views.py
@@ -1,6 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import ContactForm
+from .models import Contact
 
-# Create your views here.
+# show all contact
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, 'home.html', {'contacts': contacts})
 
-def home_view(request):
-    return render(request,'home.html')
+# add contact in user
+def add_contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_list')
+    else:
+        form = ContactForm()
+    return render(request, 'add_contact.html', {'form': form})
+
+# edit contact in user
+def edit_contact(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_list')
+    else:
+        form = ContactForm(instance=contact)
+    return render(request, 'add_contact.html', {'form': form})
+
+
+# delete contact in user
+def delete_contact(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('contact_list')
+    return render(request, 'contact_confirm_delete.html', {'contact': contact})
+
+
+# show contact in admin
+def admin_Dashboard(request):
+    contacts = Contact.objects.all()
+    return render(request,'admin_home.html',{'contacts': contacts})
+
+# add contact in admin
+def admin_contact(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('admin_dashboard')
+    return render(request, 'contact_delete.html', {'contact': contact})
+
+
+# edit contact in admin
+def edit_admin(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_dashboard')
+    else:
+        form = ContactForm(instance=contact)
+    return render(request, 'add_contact.html', {'form': form})
+
